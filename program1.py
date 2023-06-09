c816a112c33c98da31b91a557cf09da4677709d1
shanvi <sangeetabenrathodji@gmail.com>
2023-06-10 01:10:59 +0530

    git commit -m "programme1"

--- program1.py
+++ program1.py
@@ -1,3 +1,9 @@
-print("hellow World!")
-print("hellow shanvi kesi ho")
-print("tera mujse pehle ka nata koi.... yuhi nhi dil lubhata koi...")
+# write a python programme to check given umber is "positive" "nagative" or "zero"
+number=int(input("Enter number: "))
+if number > 0:
+    print("positive number")
+elif number<0:
+    print("nagative number")
+else:
+    print("Zero")
+ 

0e6976a9f4a8b7b49e549150f0229438e213360d
shanvi <sangeetabenrathodji@gmail.com>
2023-06-14 03:15:17 +0530

    add reverse num program

--- WhileLoop/Program-to-Print-First-N-Natural-Numbers.py
+++ WhileLoop/Program-to-Print-First-N-Natural-Numbers.py
@@ -5,9 +5,24 @@ while a<=10:
     print(a)
     a=a+1
 
-    # Program to Print First N Natural Numbers
-    i=1
-    n=int(input("Give Ending Loop N number: "))
-    while i<=n:
-        print(i)
-        i=i+1
+# Program to Print First N Natural Numbers
+i=1
+n=int(input("Give Ending Loop N number: "))
+while i<=n:
+    print(i)
+    i=i+1
+    
+
+# Print First N Natural Number in Reverse Order
+
+b=1
+n=int(input("Give strating Loop N number: "))
+while b<=n:
+    print(n)
+    n=n-1
+
+    # =================== OR=====================
+i= int(input("enter num where to start"))
+while i>=1:
+    print(i)
+    i=i-1
